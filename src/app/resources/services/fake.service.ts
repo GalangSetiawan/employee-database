219ef0a795b857984f590213344096da9b625316
Galang Setiawan <52562711+GalangSetiawan@users.noreply.github.com>
2024-03-29 09:53:16 +0000

    s

--- src/app/resources/services/fake.service.ts
+++ src/app/resources/services/fake.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SessionHelper } from '../helper/session-helper';
+import { isDevMode } from '@angular/core';
 
 @Injectable()
 export class FakeService {
 
   public isUserLogin = false;
+  public URL_BASE_FOR_DEPLOY = "/employee-database";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    if(isDevMode()){
+      this.URL_BASE_FOR_DEPLOY = '';
+    }else{
+      this.URL_BASE_FOR_DEPLOY = "/employee-database";
+    }
+
+    console.log('isDevMode ==>',isDevMode(), this.URL_BASE_FOR_DEPLOY)
+
+  }
 
   public checkIsUserLogin(){
     var isLogin = SessionHelper.getItem('USER_HAS_LOGGED_IN');
@@ -27,35 +38,35 @@ export class FakeService {
   }
 
   public getFakeLoginCredential() {
-    return this.http.get('/assets/fake-data-json/login-credential.json');
+    return this.http.get(this.URL_BASE_FOR_DEPLOY + '/assets/fake-data-json/login-credential.json');
   }
 
   public getFakeEmployee() {
-    return this.http.get('/assets/fake-data-json/employee-data.json');
+    return this.http.get(this.URL_BASE_FOR_DEPLOY + '/assets/fake-data-json/employee-data.json');
   }
 
   public getStatusPtkp() {
-    return this.http.get('/assets/fake-data-json/status-ptkp.json');
+    return this.http.get(this.URL_BASE_FOR_DEPLOY + '/assets/fake-data-json/status-ptkp.json');
   }
 
   public getStatusPernikahan() {
-    return this.http.get('/assets/fake-data-json/status-pernikahan.json');
+    return this.http.get(this.URL_BASE_FOR_DEPLOY + '/assets/fake-data-json/status-pernikahan.json');
   }
 
   public getJenisKelamin() {
-    return this.http.get('/assets/fake-data-json/gender.json');
+    return this.http.get(this.URL_BASE_FOR_DEPLOY + '/assets/fake-data-json/gender.json');
   }
 
   public getStatusKepegawaian() {
-    return this.http.get('/assets/fake-data-json/employee-status.json');
+    return this.http.get(this.URL_BASE_FOR_DEPLOY + '/assets/fake-data-json/employee-status.json');
   }
 
   public getAgama() {
-    return this.http.get('/assets/fake-data-json/agama.json');
+    return this.http.get(this.URL_BASE_FOR_DEPLOY + '/assets/fake-data-json/agama.json');
   }
 
   public getDepartemen() {
-    return this.http.get('/assets/fake-data-json/departemen.json');
+    return this.http.get(this.URL_BASE_FOR_DEPLOY + '/assets/fake-data-json/departemen.json');
   }
 
 
